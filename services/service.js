4bef4b552247d700eb295972544448ec877aa36e
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2025-12-13 23:44:34 +0530

    fix

--- services/service.js
+++ services/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const ConstantsUrl = "http://localhost:3002/";
-// const ConstantsUrl = "";
+// const ConstantsUrl = "http://localhost:3002/";
+const ConstantsUrl = "https://find-my-stay-backend2.onrender.com/";
 
 function Api(method, url, data, router, params) {
   return new Promise(function (resolve, reject) {
@@ -20,7 +20,6 @@ function Api(method, url, data, router, params) {
         resolve(res.data);
       },
       (err) => {
-
         if (err.response) {
           const status = err.response.status;
           const message = err.response?.data?.message || "";
@@ -67,7 +66,6 @@ function ApiFormData(method, url, data, router) {
         resolve(res.data);
       },
       (err) => {
-
         if (err.response) {
           if (err?.response?.status === 401) {
             if (typeof window !== "undefined") {
@@ -84,142 +82,6 @@ function ApiFormData(method, url, data, router) {
   });
 }
 
-function ApiGetPdf(url, data, router, params) {
-  return new Promise(function (resolve, reject) {
-    let token = "";
-    if (typeof window !== "undefined") {
-      token = localStorage?.getItem("token") || "";
-    }
-
-    axios({
-      method: "POST",
-      url: ConstantsUrl + url,
-      data,
-      headers: { Authorization: `jwt ${token}` },
-      params,
-      responseType: "blob",
-    }).then(
-      (res) => {
-        const file = new Blob([res.data], { type: "application/pdf" });
-        const fileURL = window.URL.createObjectURL(file);
-        const link = document.createElement("a");
-        link.href = fileURL;
-        link.setAttribute("download", `bachhoahouston-invoice-${data.id}`);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        resolve(res.data);
-      },
-      (err) => {
-
-        if (err.response) {
-          const status = err.response.status;
-          const message = err.response?.data?.message || "";
-
-          if (
-            (status === 401 || status === 403) &&
-            typeof window !== "undefined"
-          ) {
-            if (
-              message.toLowerCase().includes("jwt expired") ||
-              message.toLowerCase().includes("unauthorized")
-            ) {
-              localStorage.removeItem("token");
-              localStorage.removeItem("userDetail");
-              router?.push("/signIn");
-            }
-          }
-          reject(err.response.data);
-        } else {
-          reject(err);
-        }
-      }
-    );
-  });
-}
-
-// const timeSince = (date) => {
-//   date = new Date(date);
-//   const diff = new Date().valueOf() - date.valueOf();
-//   const seconds = Math.floor(diff / 1000);
-//   var interval = seconds / 31536000;
-
-//   if (interval > 1) {
-//     return Math.floor(interval) + " Years";
-//   }
-//   interval = seconds / 2592000;
-//   if (interval > 1) {
-//     return (
-//       Math.floor(interval) +
-//       (Math.floor(interval) > 1 ? " Months" : " Month") +
-//       " ago"
-//     );
-//   }
-//   interval = seconds / 604800;
-//   if (interval > 1) {
-//     return (
-//       Math.floor(interval) +
-//       (Math.floor(interval) > 1 ? " Weeks" : " Week") +
-//       " ago"
-//     );
-//   }
-
-//   interval = seconds / 86400;
-//   if (interval > 1) {
-//     return (
-//       Math.floor(interval) +
-//       (Math.floor(interval) > 1 ? " Days" : " Day") +
-//       " ago"
-//     );
-//   }
-//   interval = seconds / 3600;
-//   if (interval > 1) {
-//     return (
-//       Math.floor(interval) +
-//       (Math.floor(interval) > 1 ? " Hours" : " Hour") +
-//       " ago"
-//     );
-//   }
-//   interval = seconds / 60;
-//   if (interval > 1) {
-//     return (
-//       Math.floor(interval) +
-//       (Math.floor(interval) > 1 ? " Min" : " min") +
-//       " ago"
-//     );
-//   }
-//   return "Just now";
-// };
-
-const pdfDownload = async (fileName, data) => {
-  return new Promise(function (resolve, reject) {
-    pdfMake.vfs = {};
-    pdfMake.jszip = jszip;
-    pdfMake.DynamicContent = {
-      content: {
-        widths: "100%",
-      },
-    };
-    pdfMake.fonts = {
-      Roboto: {
-        normal:
-          "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
-        bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
-        italics:
-          "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
-        bolditalics:
-          "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
-      },
-    };
-
-    pdfmake.createPdf(data).download(fileName);
-    pdfmake.createPdf(data).getDataUrl((blob) => {
-
-      resolve(blob);
-    });
-  });
-};
-
 const replaceUrl = (url) => {
   return url?.replace(
     "https://surfacegallery.s3.eu-north-1.amazonaws.com",
@@ -227,4 +89,4 @@ const replaceUrl = (url) => {
   );
 };
 
-export { Api, pdfDownload, ApiFormData, replaceUrl, ApiGetPdf };
+export { Api, ApiFormData, replaceUrl };
